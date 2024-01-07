3706d1f6b8cdd21dad0f48eadc051f1e58165dc7
Md Shamir Raza Quraishi <143636691+Md-Shamir-raza@users.noreply.github.com>
2024-01-07 18:55:17 +0530

    lec4.cpp

---
+++ lec4.cpp
@@ -0,0 +1,24 @@
+//usage of different types of data types and variables
+#include<iostream>
+using namespace std;
+
+void sum(){
+   int glo=88;
+   cout<<glo;
+}
+int main()
+{
+    int glo=66;
+    bool is_true=false;
+    int a=4 , b=44;
+    char name='a';
+    float pi=3.14;
+    cout<<"the valuse of "<< is_true;
+    cout<<"\nThe values of the pi is :"<<pi;
+    cout<<"\nThe name is :"<<name;
+    cout<<"\nThe values of a is :"<<a<<"\nThe values of b is: "<<b<<"\n";
+    cout<<"\nThe glooo:"<<glo;
+      sum();
+    //cout<<"\nThe valuse off"
+    return 0;
+}
