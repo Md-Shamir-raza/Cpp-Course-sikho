0a6046d7040e08182080d8b49be9b4586baa95d2
Md Shamir Raza Quraishi <143636691+Md-Shamir-raza@users.noreply.github.com>
2024-01-07 19:02:23 +0530

    lec11.cpp

---
+++ lec11.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i < 40; i++)
+    {
+        //if (i==2)
+        //{
+        //    /* code */
+        //    break;
+        //}
+        
+        //cout<<i<<endl;/* code */
+       //if (i==2)
+       //{
+       //    /* code */
+       //    break;
+       //}
+       //
+       if (i==2)
+       {
+        /* code */
+        continue;
+       }
+       cout<<i<<endl;
+       
+    }
+    
+    return 0;
+}
